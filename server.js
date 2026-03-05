706a23eb97d8cb3b2ec66f2601f8491aa9b10569
ammonmerrell <ammonmerrell@outlook.com>
2026-03-05 13:00:45 -0700

    added express to server

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const http = require('node:http');
-const hostname = 'localhost';
-const port = 8080;
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('hello World');
+// const http = require('node:http');
+// const hostname = 'localhost';
+// const port = 8080;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('hello World');
     
     
-});
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}/professional`);
-});
+// server.listen(port, hostname, () => {
+//     console.log(`server running at http://${hostname}:${port}/professional`);
+// });

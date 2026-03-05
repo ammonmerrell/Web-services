efd2420dd9a0183c0fd3c392eaaa5856799ca495
ammonmerrell <ammonmerrell@outlook.com>
2026-03-05 13:52:36 -0700

    added server info

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'))
+
+app.listen(port, () => (console.log(`running on port ${port}`)));
 
 // const http = require('node:http');
 // const hostname = 'localhost';

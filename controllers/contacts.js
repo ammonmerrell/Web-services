a9ccb2d58574116c32615dd77a673c8577b4b8c4
ammonmerrell <ammonmerrell@outlook.com>
2026-03-07 18:20:44 -0700

    corrected '" error

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database')
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req,res) => {
-    const result = await mongodb.getDatabase().db('data0').collection("contacts").find();
+    const result = await mongodb.getDatabase().db('data0').collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);

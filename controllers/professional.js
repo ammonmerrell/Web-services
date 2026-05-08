d963cfb7f2e0c4975d79ee36d61f97d54987989d
ammonmerrell <ammonmerrell@outlook.com>
2026-05-08 17:55:18 -0600

    changed like to referende collection outside of contacts

--- controllers/professional.js
+++ controllers/professional.js
@@ -2,7 +2,7 @@ const mongodb = require('../data/database')
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req,res) => {
-    const result = await mongodb.getDatabase('cluster0').db('data0').collection('meetW1').find();
+    const result = await mongodb.getDatabase('cluster0').db('meetW1').collection('meetW1').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
@@ -11,7 +11,7 @@ const getAll = async (req,res) => {
 
 const getSingle = async (req,res) => {
     const contactId = new ObjectId(req.params);
-    const result = await mongodb.getDatabase('cluster0').db('data0').collection('meetW1').find({ _id: contactId});
+    const result = await mongodb.getDatabase('cluster0').db('meetW1').collection('meetW1').find({ _id: contactId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);

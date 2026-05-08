5574573a4616566a36eb11b00fe5f4cf121566dd
ammonmerrell <ammonmerrell@outlook.com>
2026-05-08 12:12:38 -0600

    added [0] on single file

--- controllers/professional.js
+++ controllers/professional.js
@@ -14,7 +14,7 @@ const getSingle = async (req,res) => {
     const result = await mongodb.getDatabase('cluster0').db('data0').collection('meetW1').find({ _id: contactId});
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
+        res.status(200).json(contacts[0]);
     });
 };
 

16f45317c7c7385759763ad37c9e86fa9542ecc5
ammonmerrell <ammonmerrell@outlook.com>
2026-05-08 11:38:29 -0600

    added html file

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express')
 
+
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'))
+//new additions below -------------------
 
+app.use(express.static('cse341-ww-student-code-w01-2-team-frontendStart/frontend'));
+
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + '/cse341-ww-student-code-w01-2-team-frontendStart/frontend/');
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).json();
+});
+
+//end of new additions---------------
+
+app.use('/', require('./routes'))
 
 
 mongodb.initDb((err) => {
